1737bc86d52299a2257185b860087b0f5379bd5b
Bryant Wiggins <topwigit@gmail.com>
2023-05-04 13:02:59 -0400

    going back ot finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const form = document.getElementById('create-task-form');
+
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  document.querySelector('#new-task-description').value
+
+ 
+})
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+
 });
